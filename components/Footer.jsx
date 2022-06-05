a15c3f5846022e43db7001bc319fd23b0824a1bd
shahriar-tamjid <shahriar.tamjid@outlook.com>
2022-06-05 14:58:38 +0600

    First Commit

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>&copy; 2022 Beats Headphones All Rights Reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
